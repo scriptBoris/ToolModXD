df75d4c3c0217adc66441ea27170e3437a91c73d
Boris Krit <scriptboris@gmail.com>
2019-02-07 22:35:05 +0800

    Добавьте файлы проекта.

---
+++ ToolModXdLib/Models/War3Object.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolModXdLib.Models
+{
+    public class War3Object
+    {
+        public string Art { get; set; }
+        public string Unart { get; set; }
+        public string ResearchArt { get; set; }
+        public string MissileArt { get; set; }
+        public string BuffArt { get; set; }
+        public string TargetArt { get; set; }
+        public string TargetAttach { get; set; }
+        public string TargetAttach1 { get; set; }
+        public string TargetAttach2 { get; set; }
+        public string TargetAttach3 { get; set; }
+        public string TargetAttach4 { get; set; }
+        public string TargetAttach5 { get; set; }
+        public string TargetAttachCount { get; set; }
+        public string CasterArt { get; set; }
+        public string CasterAttach { get; set; }
+        public string EffectArt { get; set; }
+        public string SpecialArt{ get; set; }
+        public string AnimNames { get; set; }
+    }
+}
---
+++ ToolModXdLib/VersionInjector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ToolModXdLib
+{
+    public class VersionInjector
+    {
+        private List<string> sourceBody;
+        private List<string> targetBody;
+
+        public VersionInjector(string pathSource, string pathTarget)
+        {
+            using (var sr = new StreamReader(pathSource) )
+            {
+                string line;
+                while( (line = sr.ReadLineAsync().Result) != null)
+                {
+                    sourceBody.Add(line);
+                }
+                sr.Close();
+            }
+
+            using (var sr = new StreamReader(pathTarget))
+            {
+                string line;
+                while ((line = sr.ReadLineAsync().Result) != null)
+                {
+                    targetBody.Add(line);
+                }
+                sr.Close();
+            }
+        }
+
+        private const string RegExPatern = "";
+
+        public void DoObjective(List<string> body)
+        {
+            foreach (var line in body)
+            {
+                var reg = new Regex(RegHeader);
+                var match = reg.Matches()
+            }
+        }
+
+        public async Task Inject()
+        {
+            
+        }
+    }
+}
