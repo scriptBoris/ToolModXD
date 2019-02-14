4f7d9da75d0e2d33e989b85fd10b468c9345f117
Boris Krit <scriptboris@gmail.com>
2019-02-14 23:57:40 +0800

    listfile теперь генерируется с учетом погрешнестей и элемантами предсказаний

--- ToolModXdLib/Core/ListFileInjector.cs
+++ ToolModXdLib/Core/ListFileInjector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ToolModXdLib.Models;
 
@@ -18,17 +19,59 @@ namespace ToolModXdLib.Core
             _toolMod = toolMod;
         }
 
-        public bool Add(string newData)
+        public void Add(string innerData)
         {
-            var match = Datas.FirstOrDefault( x => string.Equals(newData, x, StringComparison.OrdinalIgnoreCase));
-            if (match == null)
-            {
-                _toolMod.InvokeMessage($"extract listfile: \"{newData}\"");
-                Datas.Add(newData);
-            }
+            // Чистка данных от ""
+            var regWraper = new Regex("\\A\"(.*)\"\\Z");
+            var regWraperMatch = regWraper.Match(innerData);
+            if (regWraperMatch.Success)
+                innerData = regWraperMatch.Groups[2].Value;
+
+            // Если данные пришли через Запятую (текстуры)
+            var listNewData = new List<string>();
+            var split = innerData.Split(',');
+            if (split.Length > 0)
+                listNewData = new List<string>(split);
             else
-                return false;
-            return true;
+                listNewData.Add(innerData);
+
+            foreach (var item in listNewData)
+            {
+                string newData = item;
+                bool isTexture = false;
+
+                // Если данные без расширений
+                var regex = new Regex(@"(.*\.mdx|.*\.mdl|.*\.blp|.*\.tga)", RegexOptions.IgnoreCase);
+                var regMatch = regex.Match(newData);
+                if (regMatch.Success == false)
+                {
+                    var regexBtn = new Regex(@"ReplaceableTextures.*");
+                    var matchBtn = regexBtn.Match(newData);
+                    // Если это текстура
+                    if (matchBtn.Success)
+                        isTexture = true;
+                    // Если это модель
+                    else
+                        isTexture = false;
+                }
+
+                // Определяем колизии
+                var match = Datas.FirstOrDefault( x => string.Equals(newData, x, StringComparison.OrdinalIgnoreCase));
+                if (match == null)
+                {
+                    _toolMod.InvokeMessage($"extract listfile: \"{newData}\"");
+                    if (isTexture)
+                    {
+                        Datas.Add(newData + ".blp");
+                        Datas.Add(newData + ".tga");
+                    }
+                    else
+                    {
+                        Datas.Add(newData + ".mdl");
+                        Datas.Add(newData + ".mdx");
+                    }
+                }
+            }
         }
 
         public async void SaveResult(string path)
